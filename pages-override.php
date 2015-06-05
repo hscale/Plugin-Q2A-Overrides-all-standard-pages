0464390c129c44315683bf05b1af9c4e9fde22e3
hscale <hoanganhhung.vp@gmail.com>
2015-06-06 03:36:02 +0700

    add default function of Q2A 1.7.x

--- pages-override.php
+++ pages-override.php
@@ -1,5 +1,59 @@
 <?php
- 
+ /**Default function of Q2A 1.7.x
+function qa_page_routing()
+{
+	
+    $pages = array(
+			'account' => 'pages/account.php',
+			'activity/' => 'pages/activity.php',
+			'admin/' => 'pages/admin/admin-default.php',
+			'admin/approve' => 'pages/admin/admin-approve.php',
+			'admin/categories' => 'pages/admin/admin-categories.php',
+			'admin/flagged' => 'pages/admin/admin-flagged.php',
+			'admin/hidden' => 'pages/admin/admin-hidden.php',
+			'admin/layoutwidgets' => 'pages/admin/admin-widgets.php',
+			'admin/moderate' => 'pages/admin/admin-moderate.php',
+			'admin/pages' => 'pages/admin/admin-pages.php',
+			'admin/plugins' => 'pages/admin/admin-plugins.php',
+			'admin/points' => 'pages/admin/admin-points.php',
+			'admin/recalc' => 'pages/admin/admin-recalc.php',
+			'admin/stats' => 'pages/admin/admin-stats.php',
+			'admin/userfields' => 'pages/admin/admin-userfields.php',
+			'admin/usertitles' => 'pages/admin/admin-usertitles.php',
+			'answers/' => 'pages/answers.php',
+			'ask' => 'pages/ask.php',
+			'categories/' => 'pages/categories.php',
+			'comments/' => 'pages/comments.php',
+			'confirm' => 'pages/confirm.php',
+			'favorites' => 'pages/favorites.php',
+			'favorites/questions' => 'pages/favorites-list.php',
+			'favorites/users' => 'pages/favorites-list.php',
+			'favorites/tags' => 'pages/favorites-list.php',
+			'feedback' => 'pages/feedback.php',
+			'forgot' => 'pages/forgot.php',
+			'hot/' => 'pages/hot.php',
+			'ip/' => 'pages/ip.php',
+			'login' => 'pages/login.php',
+			'logout' => 'pages/logout.php',
+			'messages/' => 'pages/messages.php',
+			'message/' => 'pages/message.php',
+			'questions/' => 'pages/questions.php',
+			'register' => 'pages/register.php',
+			'reset' => 'pages/reset.php',
+			'search' => 'pages/search.php',
+			'tag/' => 'pages/tag.php',
+			'tags' => 'pages/tags.php',
+			'unanswered/' => 'pages/unanswered.php',
+			'unsubscribe' => 'pages/unsubscribe.php',
+			'updates' => 'pages/updates.php',
+			'user/' => 'pages/user.php',
+			'users' => 'pages/users.php',
+			'users/blocked' => 'pages/users-blocked.php',
+			'users/special' => 'pages/users-special.php',
+		);
+    return $pages;
+} 
+ **/
 function qa_page_routing()
 {
 	
